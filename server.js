d6f9c8ac443c3e9b43268887eeacfeeb98b779c3
FMOTRDev2025 <ian.ferrer@familymedicinerockies.com>
2025-08-26 00:16:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-{
-  "scripts": {
-    "start": "node server.js",
-    "dev": "nodemon server.js"
-  }
-}
+// server.js
+import express from "express";
+import SDK from "./.api/apis/spruce/index.js";
+
+const app = express();
+const sdk = new SDK();
+
+app.get("/ping", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Service running on port", process.env.PORT || 3000);
+});
